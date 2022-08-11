776edf9aa1bb5387133c8d81e1de5b313e3eb6c4
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2022-08-11 21:43:46 +0530

    Create Bank Account Summary II.sql

---
+++ Bank Account Summary II.sql
@@ -0,0 +1,38 @@
+Table: Users
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| account      | int     |
+| name         | varchar |
++--------------+---------+
+account is the primary key for this table.
+Each row of this table contains the account number of each user in the bank.
+There will be no two users having the same name in the table.
+ 
+
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| trans_id      | int     |
+| account       | int     |
+| amount        | int     |
+| transacted_on | date    |
++---------------+---------+
+trans_id is the primary key for this table.
+Each row of this table contains all changes made to all accounts.
+amount is positive if the user received money and negative if they transferred money.
+All accounts start with a balance of 0.
+
+
+
+SQL qury for above satatemnt
+
+SELECT u.NAME ,SUM(t.amount) AS BALANCE
+FROM Users as u
+INNER JOIN  Transactions AS t 
+WHERE u.account= t.account
+GROUP BY t.account
+HAVING BALANCE>=10000
