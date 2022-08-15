11458c8f18df3fb732857a26bc6045d85e88069a
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2022-08-15 11:00:00 +0530

    Create Employees With Missing Information.sql

---
+++ Employees With Missing Information.sql
@@ -0,0 +1,38 @@
+Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+
+
+SQL QUERY FOR ABOVE STATEMENT:
+
+SELECT employee_id FROM Employees WHERE employee_id NOT IN (SELECT employee_id FROM salaries WHERE salary IS NOT NULL) 
+    UNION 
+SELECT employee_id FROM salaries WHERE employee_id NOT IN (SELECT employee_id FROM Employees WHERE name IS NOT NULL) 
+    ORDER BY employee_id;
