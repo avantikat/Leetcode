20fb00ca5cff04634c291cb3e05c506dbee108b3
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2022-08-23 11:04:24 +0530

    Create Department Highest Salary.sql

---
+++ Department Highest Salary.sql
@@ -0,0 +1,40 @@
+Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table.
+departmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of a department and its name.
+ 
+
+Write an SQL query to find employees who have the highest salary in each of the departments.
+
+Return the result table in any order.
+
+
+
+
+SQL QUERY FRO ABOVE STATEMENT:
+
+
+SELECT d.name AS 'Department'  ,e.name AS 'Employee' , e.salary  AS 'salary'
+FROM Department d JOIN Employee e ON d.id = e.departmentId
+WHERE (e.salary,e.departmentId) IN (SELECT MAX(salary),departmentId FROM Employee GROUP BY departmentId)
